f54654aa8addc9d010cadcba543cc34dec1145a6
belovezhalin <angelina.belovezha@gmail.com>
2025-01-12 20:54:13 +0100

    Built backend view to process form data and added fetch API POST request

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
 import json
+import datetime
 
 def store(request):
     if request.user.is_authenticated:
@@ -71,3 +72,20 @@ def updateItem(request):
         orderItem.delete()
 
     return JsonResponse("Item was added", safe=False)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+    else:
+        print("User is not logged in")
+    return JsonResponse("Payment subbmited...", safe=False)
